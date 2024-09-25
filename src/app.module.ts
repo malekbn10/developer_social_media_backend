fc04cd31a4003f6c26e2a3316c99258304ad18e7
mbnj <mbnj.mbnj1@gmail.com>
2024-09-25 02:07:09 +0100

    connecting to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, UsersModule, AuthModule],
+  imports: [ TypeOrmModule.forRoot({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "",
+    database: "socialmedia",
+    autoLoadEntities: true,
+    synchronize: true,
+    }),UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersService } from 'src/users/users.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
+import { jwtConstants } from './constants/jwt.constant';
 
 @Module({
+  imports:[UsersModule,
+    JwtModule.register({
+      global:true,
+      secret:jwtConstants.secret,
+      signOptions :{expiresIn:"1d"}
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
